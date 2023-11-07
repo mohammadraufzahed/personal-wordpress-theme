adba89a2e62a0ea537b5fef85d9c5399bb0bac6c
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-11-07 11:52:49 +0330

    Added new colors, fonts and .... to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./templates/**/*.twig"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          1: "#000000",
+          2: "#ffffff",
+          3: "#7C9D96",
+          4: "#909D9B",
+          5: "#C2D1D0",
+        },
+      },
+      maxWidth: {
+        screen: "1376px",
+      },
+      borderRadius: {
+        box: 10,
+      },
+      fontFamily: {
+        yekan: ["IRANYekanX", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
